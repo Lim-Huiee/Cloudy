125d4a62eb3e71c8330b996c57b6ffb6c41b5537
Meryl Low <68551059+meryllow@users.noreply.github.com>
2023-11-05 19:40:52 +0800

    fixed ssh tunneling for test.py

--- gitwellsoon/backend/test.py
+++ gitwellsoon/backend/test.py
@@ -1,84 +1,96 @@
-# from sshtunnel import SSHTunnelForwarder
-# from flask import Flask, jsonify, request
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_jwt_extended import JWTManager
-# from flask_cors import CORS
+from sshtunnel import SSHTunnelForwarder
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# # Create an SSH tunnel
+# Create an SSH tunnel
 # with SSHTunnelForwarder(
-#     ('ec2-18-142-239-91.ap-southeast-1.compute.amazonaws.com'),
+#     ssh_address_or_host=('ec2-18-142-239-91.ap-southeast-1.compute.amazonaws.com'),
 #     ssh_username="ec2-user",
+#     ssh_password="",
 #     ssh_pkey="gitwellsoon-is458.pem",
-#     remote_bind_address=('gitwellsoon.cqglyyh818jk.ap-southeast-1.rds.amazonaws.com', 3306)
+#     remote_bind_address=('gitwellsoon.cqglyyh818jk.ap-southeast-1.rds.amazonaws.com', 3306),
+#     local_bind_address=("127.0.0.1", 63467)
 # ) as tunnel:
 #     print("****SSH Tunnel Established****")
-    
-#     # Define your Flask app configuration for the database
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:Hellocloudy!@127.0.0.1:{}/gitwellsoon'.format(tunnel.local_bind_port)
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+tunnel = SSHTunnelForwarder(
+    ('ec2-18-142-239-91.ap-southeast-1.compute.amazonaws.com', 22),
+    ssh_username="ec2-user",
+    ssh_password="",
+    ssh_pkey="gitwellsoon-is458.pem",
+    remote_bind_address=('gitwellsoon.cqglyyh818jk.ap-southeast-1.rds.amazonaws.com', 3306),
+    )
 
-#     # Create a SQLAlchemy database instance
-#     db = SQLAlchemy(app)
+tunnel.start()
+print("SSH connection created..")
 
-#     # Your database models (Accounts, Products, Orders) and their definitions go here
-#     class Accounts(db.Model):
-#         __tablename__ = "accounts"
+# Define your Flask app configuration for the database
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://admin:Hellocloudy!@127.0.0.1:{}/gitwellsoon".format(tunnel.local_bind_port)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#         username = db.Column(db.String(255), nullable=False)
-#         password = db.Column(db.String(255), nullable=False)
-#         email = db.Column(db.String(255), primary_key=True)
-#         Orders = db.relationship('Orders', backref='accounts', lazy=True, passive_deletes=True)
+# Create a SQLAlchemy database instance
+db = SQLAlchemy(app)
 
-#         def __init__(self, username, password, email):
-#             self.username = username
-#             self.password = password
-#             self.email = email
+# Your database models (Accounts, Products, Orders) and their definitions go here
+class Accounts(db.Model):
+    __tablename__ = "accounts"
 
-#     class Products(db.Model):
-#         __tablename__ = "products"
-#         def __init__(self, pcat, pname, pdesc, img_src, pprice, stock, prod_date, expiry_date):
-#             self.pcat = pcat
-#             self.pname = pname
-#             self.pdesc = pdesc
-#             self.img_src = img_src
-#             self.pprice = pprice
+    username = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), primary_key=True)
+    Orders = db.relationship('Orders', backref='accounts', lazy=True, passive_deletes=True)
 
-#             self.stock = stock
-#             self.prod_date = prod_date
-#             self.expiry_date = expiry_date
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
 
-#         pid = db.Column(db.Integer, primary_key=True)
-#         pcat = db.Column(db.String(255), nullable=False)
-#         pname = db.Column(db.String(255), nullable=False)  
-#         pdesc = db.Column(db.String(255), nullable=False)  
-#         img_src = db.Column(db.String(255), nullable=False)  
-#         pprice = db.Column(db.Integer, nullable=False)  
-#         stock = db.Column(db.Integer, nullable=False)  
-#         prod_date = db.Column(db.Date, nullable=False)
-#         expiry_date = db.Column(db.Date, nullable=False)
-#         Orders = db.relationship('Orders', backref='products', lazy=True, passive_deletes=True)
+class Products(db.Model):
+    __tablename__ = "products"
+    def __init__(self, pcat, pname, pdesc, img_src, pprice, stock, prod_date, expiry_date):
+        self.pcat = pcat
+        self.pname = pname
+        self.pdesc = pdesc
+        self.img_src = img_src
+        self.pprice = pprice
 
-#     class Orders(db.Model):
-#         __tablename__ = "orders"
-#         def __init__(self, oid, email, pid, quantity):
-#             self.oid = oid
-#             self.email = email
-#             self.pid = pid
-#             self.quantity = quantity
+        self.stock = stock
+        self.prod_date = prod_date
+        self.expiry_date = expiry_date
 
-#         oid = db.Column(db.Integer, primary_key=True)
-#         email = db.Column(db.String(255), db.ForeignKey('accounts.email'),nullable=False)  
-#         pid = db.Column(db.Integer, db.ForeignKey('products.pid'), nullable=False)
-#         quantity = db.Column(db.Integer, nullable=False)
+    pid = db.Column(db.Integer, primary_key=True)
+    pcat = db.Column(db.String(255), nullable=False)
+    pname = db.Column(db.String(255), nullable=False)  
+    pdesc = db.Column(db.String(255), nullable=False)  
+    img_src = db.Column(db.String(255), nullable=False)  
+    pprice = db.Column(db.Integer, nullable=False)  
+    stock = db.Column(db.Integer, nullable=False)  
+    prod_date = db.Column(db.Date, nullable=False)
+    expiry_date = db.Column(db.Date, nullable=False)
+    Orders = db.relationship('Orders', backref='products', lazy=True, passive_deletes=True)
 
-#     @app.route('/accounts', methods=['GET'])
-#     def get_all_accounts():
-#         accounts = Accounts.query.all()
-#         account_list = [{'username': acc.username, 'email': acc.email} for acc in accounts]
-#         return jsonify(account_list)
+class Orders(db.Model):
+    __tablename__ = "orders"
+    def __init__(self, oid, email, pid, quantity):
+        self.oid = oid
+        self.email = email
+        self.pid = pid
+        self.quantity = quantity
+
+    oid = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), db.ForeignKey('accounts.email'),nullable=False)  
+    pid = db.Column(db.Integer, db.ForeignKey('products.pid'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+@app.route('/accounts', methods=['GET'])
+def get_all_accounts():
+    accounts = Accounts.query.all()
+    account_list = [{'username': acc.username, 'email': acc.email} for acc in accounts]
+    return jsonify(account_list)
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)

efbbabba3e623fcc6d4dd6e5ba125f7af4813699
huijun-zheng <huijun.cz@gmail.com>
2023-11-06 19:25:06 +0800

    Delete aws-config-bucket.js

--- gitwellsoon/src/aws-config-bucket.js
+++
@@ -1,9 +0,0 @@
-import AWS from 'aws-sdk';
-
-AWS.config.update({
-  accessKeyId: 'AKIAUU3QCWRZY4745T62',      // Replace with your Access Key ID
-  secretAccessKey: 'l6iCK1kYwGmViPkSKcclrfr6TkJuy2gG0XIHof5Z',  // Replace with your Secret Access Key
-  region: 'ap-southeast-1',                    // Replace with your desired AWS region
-});
-
-export default AWS;
